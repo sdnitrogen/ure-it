b8f8fda64c1bf50e85b1747a662eaec914ec6147
sdnitrogen <skd.bwn96@gmail.com>
2024-03-15 01:53:52 -0700

    feat: complete first build

---
+++ README.md
@@ -0,0 +1,32 @@
+## U're It
+
+A luck based party game played between friends to select the fortunate/unfortunate one - built with Vue, TypeScript and Tailwind CSS.
+
+Check it out here: [https://better-url.vercel.app/](https://better-url.vercel.app/)
+
+### Features
+
+- 4x4 grid
+- Select the mode to find the lucky one or the unlucky one
+- [ ] grid size selection
+
+### Technologies
+
+![TypeScript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)
+![Vue](https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D)
+![Tailwind CSS](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)
+
+### How to run
+
+- Clone this repository to your local system.
+- cd to the project directory on terminal and type `npm install` followed by `npm run dev`
+- Go to url: http://localhost:5173 in your browser to check out the app.
+
+### Preview
+
+<img src="Screenshot.png" align="center"><br><br>
+
+### Support Development
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/sdnitrogen) <br>
+[![Donate](https://img.shields.io/badge/Donate-Paypal-blue?style=for-the-badge)](https://www.paypal.me/sdnitrogen)
---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import { ref, computed } from "vue"
+
+const gamestate = ref(0)
+const lucky = ref(false)
+const notit = computed(() => (lucky.value ? "😭" : "😁"))
+const it = computed(() => (lucky.value ? "😁" : "😭"))
+const count = ref(16)
+const endMsg = computed(() =>
+  lucky.value ? "Congratulations! You're it! 🥳" : "You're it! 🫵😆"
+)
+
+const setLucky = () => (lucky.value = true)
+const setUnlucky = () => (lucky.value = false)
+
+const board = ref([
+  ["", "", "", ""],
+  ["", "", "", ""],
+  ["", "", "", ""],
+  ["", "", "", ""],
+])
+
+const makeMove = (x, y) => {
+  if (board.value[x][y] !== "") return
+  gamestate.value = 1
+  const roll = Math.floor(Math.random() * count.value)
+
+  if (roll === 0) {
+    board.value[x][y] = it.value
+    count.value = 16
+    gamestate.value = 2
+    return
+  }
+  board.value[x][y] = notit.value
+  count.value -= 1
+}
+
+const resetGame = () => {
+  board.value = [
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+  ]
+  gamestate.value = 0
+}
+</script>
+
+<template>
+  <main class="pt-8 text-center">
+    <h1 class="mb-4 text-3xl font-bold uppercase">U're it</h1>
+    <div class="inline-flex text-xl mb-8">
+      <p>I'm feeling&nbsp;</p>
+      <span
+        class="cursor-pointer"
+        :class="{
+          'underline italic font-bold': lucky,
+          'pointer-events-none': gamestate !== 0,
+        }"
+        @click="setLucky"
+        >lucky</span
+      >
+      <p>&nbsp;/&nbsp;</p>
+      <span
+        class="cursor-pointer"
+        :class="{
+          'underline italic font-bold': !lucky,
+          'pointer-events-none': gamestate !== 0,
+        }"
+        @click="setUnlucky"
+        >unlucky</span
+      >
+    </div>
+    <div class="flex flex-col items-center mb-8">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <div
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="makeMove(x, y)"
+          class="border border-white flex items-center justify-center w-20 h-20 hover:bg-gray-500 text-4xl cursor-pointer"
+          :class="gamestate === 2 && 'pointer-events-none'">
+          {{ cell }}
+        </div>
+      </div>
+    </div>
+    <h2 v-if="gamestate === 2" class="text-4xl font-bold mb-8">{{ endMsg }}</h2>
+    <button
+      @click="resetGame"
+      class="px-4 py-2 border-none bg-red-500 hover:bg-red-600 rounded uppercase font-bold duration-300">
+      Reset Game
+    </button>
+  </main>
+</template>
+
+<style scoped></style>
